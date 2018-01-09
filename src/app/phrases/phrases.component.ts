6c29904886cd1873e0d946c7b8e5cb8b1663ae1c
matthew <ml.davis195@gmail.com>
2018-01-08 21:13:13 -0500

    Created initial browsing page.
    Created http request to read mock data from a json file in assets. I
    then display that data to a user and allow him to select his units with
    unit selection buttons at the bottom of the page.

--- src/app/phrases/phrases.component.ts
+++ src/app/phrases/phrases.component.ts
@@ -1,4 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface Phrase {
+  transliteration: string;
+  english: string;
+  farsi: string;
+}
+
+interface Phrases {
+  phrases: Phrase[];
+}
 
 @Component({
   selector: 'app-phrases',
@@ -7,9 +18,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PhrasesComponent implements OnInit {
 
-  constructor() { }
+  phrases: Phrase[];
+  currentPhrases: Phrase[];
+  unitSize: number;
+  numberOfUnits: number;
+  selectedUnit: number;
 
-  ngOnInit() {
+  constructor(private http: HttpClient) {
+    this.unitSize = 10;
+    this.selectedUnit = 1;
   }
 
+  ngOnInit(): void {
+    this.http.get<Phrases>('/assets/phrases.json').subscribe(data => {
+      this.phrases = data.phrases;
+      this.currentPhrases = this.getUnit(1);
+      this.numberOfUnits = Math.ceil(this.phrases.length / this.unitSize);
+    });
+  }
+
+  getUnit(unitNumber: number): Phrase[] {
+
+    const unitPhrases: Phrase[] = [];
+    const start = (unitNumber - 1) * this.unitSize;
+    const end = Math.min(start + this.unitSize, this.phrases.length);
+
+    for (let i = start; i < end; i++) {
+      unitPhrases.push(this.phrases[i]);
+    }
+    return unitPhrases;
+  }
+
+  decrementUnit() {
+    if (this.selectedUnit > 1) {
+      this.currentPhrases = this.getUnit(--this.selectedUnit);
+    }
+  }
+
+  incrementUnit() {
+    if (this.selectedUnit < this.numberOfUnits) {
+      this.currentPhrases = this.getUnit(++this.selectedUnit);
+    }
+  }
 }
