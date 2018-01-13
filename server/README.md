ac32b8e128f56e2202f691b8c2583416c421e4ab
matthew <ml.davis195@gmail.com>
2018-01-12 19:31:25 -0500

    Created basic Rest API in Spring.
    Created really basic Restful server with Spring. We can now make GET
    requests to retrieve data from server.

---
+++ server/README.md
@@ -0,0 +1,5 @@
+Can start server by executing ```./gradlew bootRun```
+
+Can make GET requests to the following URL: ```http://localhost:8080/greeting```
+
+If we wish to provide a new name we can add a query parameter: ```http://localhost:8080/greeting?name=Matthew```
---
+++ server/src/main/java/com/matthew/persian_tutor_server/PersianTutorServerApplication.java
@@ -0,0 +1,12 @@
+package com.matthew.persian_tutor_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersianTutorServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersianTutorServerApplication.class, args);
+	}
+}
