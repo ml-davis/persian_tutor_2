d8ce0b270aac732dc72d1f8a5c3d72fe41907b9d
matthew <ml.davis195@gmail.com>
2018-02-03 19:19:13 -0500

    Made quiz-settings page more user-friendly.
    Swapping selections on the quiz settings page in its previous state was
    quite annoying since you had to use the third option as a temporary
    selection. This change alters the behaviour to function in a more sane
    way.

--- client/src/app/language-selections/language-selections.component.ts
+++ client/src/app/language-selections/language-selections.component.ts
@@ -17,16 +17,25 @@ export class LanguageSelectionsComponent implements OnInit {
   setSelection(type: string, selection: string): void {
     if (type === 'revealed') {
       this.invigilator.setRevealedType(selection);
+
+      const revealed_index = this.getIndex(selection);
+      const answer_index = this.getIndex(this.invigilator.getAnswerType());
+      if (revealed_index < 0 || answer_index < 0) {
+        console.log('Error: setting selection');
+      }
+      this.invigilator.setAnswerType(this.types[this.getAnswerIndex(revealed_index, answer_index)]);
+
     } else if (type === 'answer') {
       this.invigilator.setAnswerType(selection);
+
     } else {
-      console.log('Error: unknown type selected when setting selection');
+      console.log('Error: Unknown type selected');
     }
   }
 
   isOpposingSelected(type: string, selection: string): boolean {
     if (type === 'revealed') {
-      return this.invigilator.getAnswerType() === selection;
+      return false;
     } else if (type === 'answer') {
       return this.invigilator.getRevealedType() === selection;
     } else {
@@ -46,5 +55,28 @@ export class LanguageSelectionsComponent implements OnInit {
 
   titleCase(str): string {
     return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
+  private types: string[] = [
+    'transliteration',
+    'english',
+    'farsi'
+  ];
+
+  private getIndex(selection: string): number {
+    for (let i = 0; i < this.types.length; i++) {
+      if (selection === this.types[i]) {
+        return i;
+      }
+    }
+    return -1;
+  }
+
+  private getAnswerIndex(revealed_index: number, hidden_index: number): number {
+    if (revealed_index === hidden_index) {
+      hidden_index = (hidden_index + 1) % 3;
+    }
+
+    return hidden_index;
   }
 }
